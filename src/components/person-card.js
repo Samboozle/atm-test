febe86948dff9aa4a96b2690801216fb27c206e9
Samboozle <samuel.a.bates@gmail.com>
2021-02-11 12:19:04 -0800

    added 'The People' section; figured out how to use graphql and gatsby-image

--- src/components/person-card.js
+++ src/components/person-card.js
@@ -1,5 +1,13 @@
 import React from "react";
+import Img from "gatsby-image";
 
-export default ({ person }) => {
-  return <></>;
+export default ({ person: { avatar, bonafides, name, role } }) => {
+  return (
+    <div className="person-card">
+      <Img fluid={ avatar.fluid } alt={ name } className="avatar" />
+      <h4>{ name }</h4>
+      <h4>{ bonafides }</h4>
+      <h5 style={{ marginTop: "auto" }}>{ role }</h5>
+    </div>
+  );
 }
--- src/content/people.js
+++ src/content/people.js
@@ -1,55 +1,53 @@
-
-
 export default [
   {
     name: "Michael Gleason",
     bonafides: "MIT, Consumer Brands, Blast",
     role: "Co-Founder, CEO",
-    image: ""
+    avatar: "michael.png"
   }, {
     name: "Walter Cruttenden",
     bonafides: "Acorns, E*Trade, Blast",
     role: "Co-Founder, Chairman",
-    image: ""
+    avatar: "walter.png"
   }, {
     name: "Dr. Richard Thaler, PhD",
     bonafides: "Nobel Prize, Economics",
     role: "Advisor",
-    image: ""
+    avatar: "richard.png"
   }, {
     name: "Palmer Luckey",
     bonafides: "Founder of Oculus VR & Anduril Industries",
     role: "Advisor",
-    image: ""
+    avatar: "palmer.png"
   }, {
     name: "M. Shadows",
     bonafides: "Avenged Sevenfold, Rockstar",
     role: "Advisor",
-    image: ""
+    avatar: "shadows.png"
   }, {
     name: "Mickey Avalon",
     bonafides: "Hip Hop Artist, Metal Core",
     role: "Advisor",
-    image: ""
+    avatar: "mickey.png"
   }, {
     name: "Pat Tenore",
     bonafides: "Founder and President of RVCA",
     role: "Advisor",
-    image: ""
+    avatar: "pat.png"
   }, {
     name: "Dr. Shlomo Benartzi, PhD",
     bonafides: "Behavioral Economist",
     role: "Advisor",
-    image: ""
+    avatar: "shlomo.png"
   }, {
     name: "Dr. Alexander Bystritsky, MD, PHD, UCLA",
     bonafides: null,
     role: "Advisor",
-    image: ""
+    avatar: "alexander.png"
   }, {
     name: "Bear D'Egidio",
     bonafides: "Founder of Hunny Group",
     role: "Advisor",
-    image: ""
+    avatar: "bear.png"
   },
 ];
